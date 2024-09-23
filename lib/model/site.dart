3bf48a3c5382cffc905cebdc1d5994e908158110
Pavel Marek <pavel.marek@enso.org>
2024-09-23 19:42:09 +0200

    Site has image

--- lib/model/site.dart
+++ lib/model/site.dart
@@ -1,19 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:winplant/model/plant.dart';
 
 class Site {
   final String name;
   final Light light;
-  final List<Plant> _plants;
+  final List<Plant> plants;
+  final AssetImage image;
 
-  Site({required this.name, required this.light})
-      : _plants = List<Plant>.empty(growable: true);
+  Site({required this.name, required this.light, AssetImage? image})
+      : plants = List<Plant>.empty(growable: true),
+    image = image ?? const AssetImage('assets/empty_image.jpg');
 
   void addPlant(Plant plant) {
-    _plants.add(plant);
+    plants.add(plant);
   }
 
   void removePlant(Plant plant) {
-    var removed = _plants.remove(plant);
+    var removed = plants.remove(plant);
     assert(removed);
   }
 }
