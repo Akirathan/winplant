9e5e666f0857435e67c0133e341defb9a0ff003d
Pavel Marek <pavel.marek@enso.org>
2024-09-29 15:04:22 +0200

    Improve SiteWidget preview

--- lib/widgets/site_widget.dart
+++ lib/widgets/site_widget.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:winplant/model/site.dart';
+import 'package:winplant/model/plant.dart';
+import 'package:winplant/routes.dart';
 import 'package:winplant/widgets/plant_info_widget.dart';
 import 'package:winplant/widgets/tags.dart';
+
 
 class SiteWidget extends StatelessWidget {
   final Site site;
@@ -10,18 +13,44 @@ class SiteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var plantWidgets =
-        site.plants.map((plant) => PlantInfoWidget(plant: plant)).toList();
+    var plantPreviews = site.plants.map((plant) => _plantPreview(context, plant)).toList();
     return Column(
+      mainAxisSize: MainAxisSize.max,
       children: <Widget>[
-        Flexible(child: Image(image: site.image)),
+        Flexible(flex: 2, child: Image(image: site.image)),
         Flexible(child: Text(site.name, style: const TextStyle(fontSize: 20))),
         Flexible(child: lightWidget(site.light)),
-        const Flexible(child: Text('Plants:')),
         Expanded(
-          child: ListView(children: plantWidgets),
+          flex: 4,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 18),
+            child: ListView(children: plantPreviews),
+          ),
         )
       ],
+    );
+  }
+
+  Widget _plantPreview(BuildContext context, Plant plant) {
+    AssetImage img = plant.image ?? plant.info.image;
+    String name = plant.name ?? plant.info.name;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+      constraints: const BoxConstraints.tightFor(height: 50, width: 200),
+      child: OutlinedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, plantRoute, arguments: plant);
+        },
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(child: Image(image: img)),
+            Flexible(child: Text(name)),
+          ],
+        )
+      ),
     );
   }
 }
