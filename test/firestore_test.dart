3ca612f460dd0076238ad418fb22e9ff70033fd7
Pavel Marek <pavel.marek@enso.org>
2025-02-10 19:37:25 +0100

    Move user_data tests to firestore_test

---
+++ test/firestore_test.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:winplant/model/user_data.dart';
+
+void main() {
+  FirebaseFirestore db = FakeFirebaseFirestore();
+
+  group('User data', () {
+    test('Test user data fetching', () async {
+      var id = 'test-user';
+      var userData = UserData(id: id, siteIds: []);
+      await userData.store(db);
+      var fetchedUserData = await UserData.fetch(db, id);
+      expect(fetchedUserData!.id, id);
+    });
+
+    test('Fetch non-existing user', () async {
+      var id = 'non-existing-user';
+      var fetchedUserData = await UserData.fetch(db, id);
+      expect(fetchedUserData, isNull);
+    });
+  });
+}
