bbc0f0963d2bdeb0d15b330d908580cbee28d44b
Pavel Marek <pavel.marek@enso.org>
2025-02-16 20:05:18 +0100

    Fix LoadingImageWidget to actually show CircularProgressIndicator

--- lib/widgets/loading_image.dart
+++ lib/widgets/loading_image.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
-class LoadingImageWidget extends StatelessWidget {
+class LoadingImageWidget extends StatefulWidget {
   final Uri url;
 
   const LoadingImageWidget({super.key, required this.url});
 
   @override
+  State<StatefulWidget> createState() => _LoadingImageWidgetState();
+}
+
+class _LoadingImageWidgetState extends State<LoadingImageWidget> {
+  bool _isFetched;
+  late Image _image;
+
+  _LoadingImageWidgetState() : _isFetched = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _image = Image.network(widget.url.toString());
+    var imgProvider = _image.image;
+    var imgStream = imgProvider.resolve(const ImageConfiguration());
+    var listener = ImageStreamListener((imgInfo, _) {
+      _isFetched = true;
+    });
+    imgStream.addListener(listener);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var image = Image.network(url.path);
-    return image;
+    if (_isFetched) {
+      return _image;
+    } else {
+      return const CircularProgressIndicator();
+    }
   }
 }
